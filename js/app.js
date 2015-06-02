9fde9f116b34f23c0e979ae9613bdc64169a7565
David Shaw <david.mi.shaw@gmail.com>
2015-06-02 18:22:43 -0400

    Setting up Backbone

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function() {
+    define(['backbone'], function(Backbone) {
+
+        'use strict';
+
+        return {
+            start: function() {
+                console.log('APP', 'start');
+            }
+        }
+
+    });
+})();
