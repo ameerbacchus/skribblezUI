d9a5d7661dc93ccd26c31142fb85a95158d348cb
Ameer Bacchus <ameerbacchus@yahoo.com>
2015-06-17 16:46:53 -0400

    playing with scrolling trigger events.

--- js/skribblez-index.js
+++ js/skribblez-index.js
@@ -8,16 +8,47 @@
 // on scroll,
 $(window).on('scroll',function(){
 
-    windowScroll = $(window).scrollTop();
-    console.log(windowScroll);
 
-    if(windowScroll>0){
+    var $windowScroll = $(window).scrollTop();
+    var $windowHeight = $(window).height();
+    var $bodyHeight = $('body').outerHeight();
+    var $starterScrollTop = $('.sk-start-wrapper').offset().top;
+//    var $starterHeight = ($('.sk-start-wrapper').outerHeight())/2;
+    var starterScrollHeight = $starterScrollTop - $windowHeight;
+//    var starterScrollTrigger = starterScrollHeight + $starterHeight;
+
+
+    var scrollComplete = $bodyHeight - $windowHeight;
+    var scrollRatio = $windowHeight/$bodyHeight;
+    var scrollThumbHeight = Math.floor(scrollRatio * $windowHeight - 20);
+
+
+    var triggerHeight = scrollThumbHeight + ($windowScroll * scrollRatio);
+    var triggerHeightPx = triggerHeight + "px";
+
+    console.log("windowScroll = " + $windowScroll);
+    console.log("starterScrollTop = " + $starterScrollTop);
+//    console.log("starterScrollTrigger = " + starterScrollTrigger);
+   console.log("triggerHeight = " + triggerHeight);
+
+    /* demo purposes only */
+    var $trigger = $('.trigger-test');
+    $trigger.css("top", triggerHeightPx);
+
+
+    if($windowScroll > 0){
         $('nav').addClass('shrink');
         $('#brand').addClass('shrink-brand');
 
     } else{
         $('nav').removeClass('shrink');
         $('#brand').removeClass('shrink-brand');
+    }
+
+
+    if(   $windowScroll > starterScrollHeight  ){
+        console.log('yes');
+        $('.sk-start-container').fadeIn("slow");
     }
 
 });
